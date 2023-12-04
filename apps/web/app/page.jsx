74ee0c4aa2b634844c636bfc79921e50218a543b
webdev-ashishk <webdev.ashishk@gmail.com>
2023-12-05 04:09:46 +0530

    updated backend and frontend both

---
+++ apps/web/app/page.jsx
@@ -0,0 +1,26 @@
+async function getData() {
+  const res = await fetch(process.env.API_URL);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+  return (
+    <div style={{ margin: "10px" }}>
+      <h1> @ home page </h1>
+      <h1>List of all users!</h1>
+      {data.map((items) => {
+        return (
+          <div key={items.id}>
+            {items.email} && {items.id}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
